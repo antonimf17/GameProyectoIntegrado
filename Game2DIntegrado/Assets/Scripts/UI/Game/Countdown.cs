f99e4c91a1d28a6d56a97238b56bfd12e8b414fc
antonimf17 <antonimoll.20novembre@gmail.com>
2025-02-12 08:59:34 +0100

    Cuenta Regresiva
    Regresiva

---
+++ Game2DIntegrado/Assets/Scripts/UI/Game/Countdown.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public int countdownTime;
+    public TMP_Text countdownDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(CountDownToStart());
+    }
+
+    IEnumerator CountDownToStart()
+    {
+        while(countdownTime > 0)
+        {
+            Time.timeScale = 0;
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSecondsRealtime(1f);
+
+            countdownTime--;
+        }
+        countdownDisplay.text = "GO!";
+        Time.timeScale = 1;
+     
+
+
+       yield return new WaitForSecondsRealtime(1f);
+        countdownDisplay.gameObject.SetActive(false);
+
+       
+
+    }
+
+
+}
